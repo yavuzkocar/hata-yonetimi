b34f1b93372d8202c36d320cafd32b1f06e56767
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-10-26 18:18:23 +0300

    hata yonetimi

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+     {
+        static void Main(string[] args)
+         {
+            try
+             {
+             Console.WriteLine("bir sayı giriniz: ");
+             int a = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("girmiş oldugunuz sayı:" + a);
+             } 
+             catch (Exception ex)
+             {
+                 Console.WriteLine("hata: " + ex.Message.ToString());
+             }
+             finally
+             {
+                 Console.WriteLine("işlem tamamlandı");
+             }
+             try
+            {
+              int a = int.Parse(null);
+              
+             }
+             catch (ArgumentNullException ex)
+             {
+             Console.WriteLine("boş bir değer girdiniz:");
+             Console.WriteLine(ex);
+        
+             }
+    
+         } 
+         
+     }    
+}
